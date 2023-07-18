96bede9515a89e720bddab6fbe7ce512c813a9ff
user <user@example.com>
2023-07-19 00:27:50 +0300

    jackson annotations added

--- app/src/main/java/ru/byum/muscat/data/network/ReleaseNetwork.kt
+++ app/src/main/java/ru/byum/muscat/data/network/ReleaseNetwork.kt
@@ -1,7 +1,11 @@
 package ru.byum.muscat.data.network
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ReleaseNetwork(
-    val id: Int,
-    val title: String,
-    val released: String,
+    @JsonProperty("id") val id: Int,
+    @JsonProperty("title") val title: String,
+    @JsonProperty("released") val released: String,
 )
